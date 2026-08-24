b25481be169b6e221cc8437cb57113124c5c78ef
Flying-IMJK <1030342707@qq.com>
2026-08-24 10:19:34 +0800

    删除无用文件

--- Src/Engine/_Ignore/Core/_Generated/AutoGenerated/IType.typeinfo.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-//*************************************************************************
-// This is an auto-generated file - DO NOT edit
-//*************************************************************************
-#include "E:/EngineProject/SolarEngine/Src/Engine//Core/TypeSystem/IType.h"
